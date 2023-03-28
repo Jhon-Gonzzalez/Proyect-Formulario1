1ac75cb2282debb52b16cf214d99c3a6658622b5
Jhon <jhong2575@gmail.com>
2023-03-28 18:25:15 -0500

    Initial commit

---
+++ src/formulario1/Formulario1.java
@@ -0,0 +1,33 @@
+/*
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formulario1;
+
+
+
+/**
+ *
+ * @author WIN-10
+ */
+public class Formulario1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+       
+        
+        Formulario_2Vista form2 =new Formulario_2Vista();
+        form2.setVisible(true);
+          Formulario_1Vista ventana = new Formulario_1Vista();
+        ventana.setVisible(true);
+        
+    }
+    
+}
